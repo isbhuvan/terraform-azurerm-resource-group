6a46f6a4b5cecc5c31416a1ed23d2d8e4f820ee0
isBhuvan <isBhuvan@outlook.com>
2023-12-08 22:02:06 +0530

    fmt config indent

--- outputs.tf
+++ outputs.tf
@@ -1,4 +1,4 @@
 output "id" {
-    value = azurerm_resource_group.this.id
-    description = "The ID of the Resource Group."
+  value       = azurerm_resource_group.this.id
+  description = "The ID of the Resource Group."
 }
